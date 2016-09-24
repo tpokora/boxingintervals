d82f1eebc992abd54a3da85ea3d3d29dee224145
tpokora <pokora.tomek@gmail.com>
2016-09-24 13:52:16 +0200

    Basic Timer and Settings Fragment

--- app/src/main/java/projects/tpokora/com/boxingintervals/MainActivity.java
+++ app/src/main/java/projects/tpokora/com/boxingintervals/MainActivity.java
@@ -1,13 +1,32 @@
 package projects.tpokora.com.boxingintervals;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import projects.tpokora.com.boxingintervals.fragments.SettingsFragment;
+import projects.tpokora.com.boxingintervals.fragments.TimerFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TimerFragment timerFragment;
+    private SettingsFragment settingsFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (findViewById(R.id.fragment_container) != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+            timerFragment = new TimerFragment();
+
+            fragmentTransaction.add(R.id.fragment_container, timerFragment).commit();
+        }
     }
 }
---
+++ app/src/main/java/projects/tpokora/com/boxingintervals/fragments/SettingsFragment.java
@@ -0,0 +1,20 @@
+package projects.tpokora.com.boxingintervals.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import projects.tpokora.com.boxingintervals.R;
+
+/**
+ * Created by pokor on 24.09.2016.
+ */
+public class SettingsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater layoutInflater, ViewGroup container, Bundle saveInstanceState) {
+        return layoutInflater.inflate(R.layout.fragment_settings, container, false);
+    }
+}
