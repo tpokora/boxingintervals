4526bbe8219a8388981af2de27572b279207caf2
tpokora <pokora.tomek@gmail.com>
2016-10-05 13:02:48 +0200

    Intervals timer

--- app/src/main/java/projects/tpokora/com/boxingintervals/fragments/TimerFragment.java
+++ app/src/main/java/projects/tpokora/com/boxingintervals/fragments/TimerFragment.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import projects.tpokora.com.boxingintervals.R;
 import projects.tpokora.com.boxingintervals.Timer;
@@ -33,14 +32,14 @@ public class TimerFragment extends AbstractFragment {
     private Handler timerHandler;
     private Runnable timerRunnable;
 
-    private boolean runTimer = false;
     private long startTime = 0L;
     private long startIntervalTime = 0L;
     private long timeInMilliseconds = 0L;
-    private long timeSwapBuff = 0L;
     private long currentTime = 0L;
     private long currentTimeSecs = 0L;
+    private long totalLeftSeconds = 0L;
     private long mins = 0L;
+    private long secondsToShow = 0L;
 
     @Override
     public View onCreateView(LayoutInflater layoutInflater, ViewGroup container, Bundle saveInstanceState) {
@@ -56,15 +55,15 @@ public class TimerFragment extends AbstractFragment {
     }
 
     public void setTimer() {
-        timer = new Timer(10, 2, 1, 10, 20);
+        timer = new Timer(2, 2, 0, 5, 20);
     }
 
     private void initTimerUI() {
         bufforTextView = (TextView) getActivity().findViewById(R.id.buffor_title);
-        bufforTextView.setText(Integer.toString(timer.getBuffor()));
+        bufforTextView.setText(String.format("%02d", timer.getBuffor()));
 
         intervalsTextView = (TextView) getActivity().findViewById(R.id.interval_counter_title);
-        intervalsTextView.setText(INTERVAL_NUMBER_STRING + " 1/" + timer.getIntervals());
+        intervalsTextView.setText(String.format("%s %d/%d", INTERVAL_NUMBER_STRING, 1, timer.getIntervals()));
 
         intervalTimerTextView = (TextView) getActivity().findViewById(R.id.interval_counter);
         intervalTimerTextView.setText(timer.getTimerTime());
@@ -80,17 +79,15 @@ public class TimerFragment extends AbstractFragment {
             boolean interval = false;
             boolean rest = false;
 
-            int intervalCounter = 0;
+            int intervalCounter = 1;
 
             @Override
             public void run() {
                 // start buffor
                 if (buffor) {
                     timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
-                    currentTime = timeSwapBuff + timeInMilliseconds;
+                    currentTime = timeInMilliseconds;
                     currentTimeSecs = (int) (currentTime / 1000);
-                    mins = currentTimeSecs / 60;
-
                     currentTimeSecs = currentTimeSecs % 60;
                     Log.d(DEBUG_TAG, "Buffor second: " + currentTimeSecs);
                     bufforTextView.setText(String.format("%02d", timer.getBuffor() - currentTimeSecs));
@@ -99,10 +96,46 @@ public class TimerFragment extends AbstractFragment {
                         buffor = false;
                         interval = true;
                         currentTime = 0;
-                        timeSwapBuff = 0;
                         currentTimeSecs = 0;
                         startTime = SystemClock.uptimeMillis();
                     }
+                }
+
+                // start interval
+                if (interval) {
+                    timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
+                    currentTime = timeInMilliseconds;
+                    currentTimeSecs = (int) (currentTime / 1000);
+                    totalLeftSeconds = timer.getIntervalDuration() - currentTimeSecs;
+                    mins = totalLeftSeconds / 60;
+                    secondsToShow = totalLeftSeconds % 60;
+
+                    String timerString = String.format("%02d:%02d", mins, secondsToShow);
+
+                    intervalTimerTextView.setText(timerString);
+
+                    // intervals check
+                    if (totalLeftSeconds == 0) {
+                        if (intervalCounter > timer.getIntervals() - 1) {
+                            interval = false;
+                            rest = true;
+                        } else {
+                            Log.d(DEBUG_TAG, "Interval counter increase");
+                            intervalCounter++;
+                            intervalsTextView.setText(String.format("%s %d/%d", INTERVAL_NUMBER_STRING, intervalCounter, timer.getIntervals()));
+                            currentTime = 0;
+                            startTime = SystemClock.uptimeMillis();
+                        }
+                    }
+                }
+
+                // start rest
+                if (rest) {
+                    // TODO: Breaks timer
+                    Log.d(DEBUG_TAG, "Starting break");
+                    intervalTimerTextView.setText(timer.getTimerTime());
+                    rest = false;
+                    //interval = true;
                 }
 
                 timerHandler.postDelayed(this, 1000);
